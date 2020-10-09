499d32e3e2dcbbe6b5b8a41824c0d7ab13c82872
Thomas Weaver <tjayweaver@gmail.com>
2020-10-08 22:39:35 -0400

    Initial commit

---
+++ src/weaver/Main.java
@@ -0,0 +1,9 @@
+package weaver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String myName = "Thomas";
+        System.out.println("Hello, my name is " + myName);
+    }
+}
