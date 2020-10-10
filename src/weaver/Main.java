c7868950c953b1506a0d503b660e445d62af0e11
Irina Ohara <irina.v.ohara@gmail.com>
2020-10-10 11:52:35 -0600

    Added new greeting

--- src/weaver/Main.java
+++ src/weaver/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         String myName = "Thomas";
+        String newName = "Irina";
         System.out.println("Hello, my name is " + myName);
+        System.out.println("Hello " + myName + ", my name is " + newName + ".");
     }
 }
