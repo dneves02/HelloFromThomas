f45a72f2205ebf0ad3e56e42c868fef2c1b6c41e
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-10-10 19:36:30 -0700

    Update Main.java

--- src/weaver/Main.java
+++ src/weaver/Main.java
@@ -5,8 +5,10 @@ public class Main {
 
         String myName = "Thomas";
         String newName = "Irina";
+        String friend = "Davi";
         System.out.println("Hello, my name is " + myName);
         System.out.println("Hello " + myName + ", my name is " + newName + ".");
         System.out.println("Hello " + myName + ", my name is Evan.");
+        System.out.println("Hi " myName + ", this is your friend " + friend + ". How are you guys doing?");
     }
 }
